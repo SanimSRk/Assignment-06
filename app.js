dd7909e4add2954c98e9487c66fdca388b3928ad
SanimSRk <hasansanim562@gmail.com>
2024-03-03 13:15:32 +0600

    click to add the value section javascript used done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const allDataLoding = async () => {
   const jsonData = await dataLode.json();
   const data = jsonData.posts;
   console.log(data);
+
   //-------add to card detalis--------
   let sum = null;
   if (data.isActive === true) {
@@ -68,16 +69,16 @@ const allDataLoding = async () => {
                       <h2>${cards.posted_time}</h2>
                     </div>
                   </div>
-                  <div class="card-actions ">
-
-                    <span class="p-[2px] border-[4px] rounded-full border-[#10B981]"><svg
+                 
+<button onclick="addDetalisShow('${cards.title}','${cards.view_count}')"  class="  p-[2px] border-[4px] rounded-full border-[#10B981]">
+                   <svg
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                           d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                       </svg>
-                    </span>
-                  </div>
+                    
+                 </button>
                 </div>
               </div>
 
@@ -89,6 +90,30 @@ const allDataLoding = async () => {
 
     cardContners.appendChild(divs);
   });
+};
+
+//click to add input section add detali
+let count = 0;
+const addToTextContns = document.getElementById('addToTextContns');
+const countValue = document.getElementById('countValue');
+const addDetalisShow = (id, view) => {
+  console.log(id);
+  count++;
+  countValue.innerText = count;
+  console.log(count);
+  const div = document.createElement('div');
+  div.classList = `my-5 rounded-lg p-4 gap-6 items-center bg-[#FFFFFF] flex justify-center`;
+  div.innerHTML = `
+    <h2 class="text-xl font-semibold">${id}</h2>
+  <p class="flex gap-2" >  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                      </svg>
+ ${view}</p>
+    `;
+  addToTextContns.appendChild(div);
 };
 
 allDataLoding();
